ae00922916613f9292513570f74b34121ebf2f55
juan <tegami2@proton.me>
2023-01-10 00:42:15 +0800

    The Phonebook （尷尬……

--- part2/The Phonebook/src/App.js
+++ part2/The Phonebook/src/App.js
@@ -1,22 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personService from './services/person.js'
+import { nanoid } from 'nanoid'
 
-const Filter = ({ val, handle }) => {
+const Notification = ({msg, style}) => {
+  if (msg === '') {
+    return null
+  } else {
+    return (
+      <div style={style}>
+        {msg}
+      </div>
+    )
+  }
+}
+
+
+const Input = ({ val, handle }) => {
   return (
-    <div>
-      filter shown with <input value={val} onChange={evt => handle(evt.target.value)} />
-    </div>
+    <>
+      <input value={val} onChange={evt => handle(evt.target.value)} />
+    </>
   )
 }
 
 
-const PersonForm = ({ val1, val2, handle1, handle2, formHandle }) => {
+const Filter = ({ val, handle }) => {
+  return (
+    <>
+      filter shown with<Input val={val} handle={handle} />
+    </>
+  )
+}
+
+
+const PersonForm = ({ formHandle, val1, val2, handle1, handle2 }) => {
   return (
     <form onSubmit={formHandle}>
       <div>
-        name: <input value={val1} onChange={evt => handle1(evt.target.value)} />
+        name: <Input val={val1} handle={handle1} />
       </div>
       <div>
-        number: <input value={val2} onChange={evt => handle2(evt.target.value)} />
+        number: <Input val={val2} handle={handle2} />
       </div>
       <div>
         <button type="submit">add</button>
@@ -26,53 +50,123 @@ const PersonForm = ({ val1, val2, handle1, handle2, formHandle }) => {
 }
 
 
-const Person = ({ persons, filter }) => {
+const Person = ({ persons, filter, setPersons }) => {
+  const remove = p => {
+    if (window.confirm(`delete ${p.name}`)) {
+      personService.remove(p.id)
+      personService.getAll()
+        .then(returnedPersons => setPersons(returnedPersons))
+    }
+  }
   return (
-    <>
-      {persons.filter(p => 
-        p.name.toLowerCase().includes(filter.toLowerCase())).map(p => 
-        <li key={p.name}>{p.name} {p.number}</li>)}
-    </>
+    <ul>
+      {persons.filter(p =>
+        p.name.toLowerCase().includes(filter.toLowerCase())).map(p =>
+          <li key={p.name}>
+            {p.name} {p.number}
+            <button onClick={() => remove(p)}>delete</button>
+          </li>)}
+    </ul>
   )
 }
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  console.log("refresh app!");
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [style, setStyle] = useState({
+    padding: '1em',      
+    marginBottom: '1em',
+
+    borderStyle: 'solid',
+    borderRadius: 10,
+    borderWidth: 3,
+
+    backgroundColor: '#d3d3d3',
+    fontSize: 16
+  })
+  const [notificationMsg, setNotificationMsg] = useState('')
+
+  useEffect(() => {
+    console.log('get data from server');
+    personService.getAll()
+      .then(returnedPersons => setPersons(returnedPersons))
+  }, [])
 
 
-  const addNameHandle = evt => {
+  const addPerson = evt => {
     evt.preventDefault()
-    if (persons.find(p => p.name === newName) === undefined) {
-      setPersons([...persons, { name: newName, number: newNumber }])
-      setNewName('')
-      setNewNumber('')
-    } else {
-      alert(`${newName} is already added to phonebook`)
+    if (newName === '') {
+      alert('please check your name')
+    }
+    else if (newNumber === '') {
+      alert('please check your number')
+    }
+    else {
+      const findedPerson = persons.find(p => p.name === newName)
+      if (findedPerson === undefined) {
+        const newOne = { name: newName, number: newNumber, id: nanoid() }
+        personService
+          .create(newOne)
+          .then(returnedPerson => {
+            console.log('add a new one to Phonebook')
+            setPersons(persons.concat(returnedPerson))
+            setStyle({...style, borderColor: 'green', color: 'green'})
+            setNotificationMsg(`Added ${returnedPerson.name}`)
+            setTimeout(() => {
+              setNotificationMsg('')
+            }, 5000);
+            setNewName('')
+            setNewNumber('')
+          })
+        } else {
+          if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
+            const newOne = { ...findedPerson, number: newNumber }
+            personService
+            .update(newOne.id, newOne)
+            .then(returnedPerson => {
+              console.log('update a old number')
+              setPersons(persons.map(p => p.id !== returnedPerson.id ? p : returnedPerson))
+              setStyle({...style, borderColor: 'green', color: 'green'})
+              setNotificationMsg(`Updated ${returnedPerson.name}`)
+              setTimeout(() => {
+                setNotificationMsg('')
+              }, 5000);
+              setNewName('')
+              setNewNumber('')
+            })
+            .catch(error => {
+              setStyle({...style, borderColor: 'red', color: 'red'})
+              setNotificationMsg(`Information of ${newName} has alread been removed from server`)
+              setTimeout(() => {
+                setNotificationMsg('')
+                setPersons(persons.filter(p => p.name !== newName))
+              }, 3000);         
+            })
+        }
+      }
     }
   }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification msg={notificationMsg} style={style}/>
       <Filter val={filter} handle={setFilter} />
       <h2>add a new</h2>
-      <PersonForm formHandle={addNameHandle}
+      <PersonForm
+        formHandle={addPerson}
         val1={newName}
         handle1={setNewName}
         val2={newNumber}
         handle2={setNewNumber}
       />
       <h2>Numbers</h2>
-      <Person filter={filter} persons={persons} />
+      <Person filter={filter} persons={persons} setPersons={setPersons} />
     </div>
   )
 }
