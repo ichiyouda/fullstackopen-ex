4b04381980d7aece9256ddf30a8834a082e870da
juan <tegami2@proton.me>
2023-01-30 21:52:31 +0800

    ex 4.3 - 4.7 : add Jest

---
+++ part4/blog-list/utils/list_helper.js
@@ -0,0 +1,44 @@
+const _ = require('lodash')
+
+
+// eslint-disable-next-line no-unused-vars
+const dummy = blogs => {
+  return 1
+}
+
+const totalLikes = blogs => {
+  return blogs.reduce((acc, blog) => acc + blog.likes, 0)
+}
+
+
+const favoriteBlog = blogs => {
+  blogs.sort((a, b) => b.likes - a.likes)
+  return blogs[0]
+}
+
+
+const mostBlogs = blogs => {
+  const Authors = _.transform(_.countBy(blogs, blog => blog.author), (res, val, key) => {
+    res.push({
+      author: key,
+      blogs: val
+    })
+  }, [])
+  return _.maxBy(Authors, a => a.blogs)
+}
+
+
+const mostLikes = blogs => {
+  const Authors = _.transform(_.groupBy(blogs, b => b.author), (res, val, key) => {
+    res.push({
+      author: key,
+      likes: val.reduce((acc, v) => acc + v.likes, 0)
+    })
+  }, [])
+  return _.maxBy(Authors, a => a.likes)
+}
+
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
